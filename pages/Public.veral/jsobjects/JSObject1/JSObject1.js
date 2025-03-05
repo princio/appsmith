95d9f466fc0e56c67db859466a596fa125db879f
Lorenzo Principi <l.principi8@gmail.com>
2025-03-05 16:16:20 +0000

    update

--- pages/Public.veral/jsobjects/JSObject1/JSObject1.js
+++ pages/Public.veral/jsobjects/JSObject1/JSObject1.js
@@ -1,47 +1,48 @@
 export default {
+	verifica: {},
 	showTimings: true,
-	valOptions: [
-    {
-        "value": 0,
-        "label": "0"
-    },
-    {
-        "value": 0.5,
-        "label": "0.5"
-    },
-    {
-        "value": 1,
-        "label": "1"
-    },
-    {
-        "value": 1.5,
-        "label": "1.5"
-    },
-    {
-        "value": 2,
-        "label": "2"
-    },
-    {
-        "value": 2.5,
-        "label": "2.5"
-    },
-    {
-        "value": 3,
-        "label": "3"
-    },
-    {
-        "value": 3.5,
-        "label": "3.5"
-    },
-    {
-        "value": 4,
-        "label": "4"
-    }
-],
 	switchShowTimings() {
 		this.showTimings = !this.showTimings;
 	},
-	bo(th) {
-		console.log(th);
+	async setVerifica() {
+		const data = await SelectVerifica.run();
+		this.verifica = data[0];
+		SelectVeral.run();
+	},
+	voto(row) {
+		let voto = 0;
+		let tot = 0;
+		for (let i = 1; i <= 6; i++) {
+			const lbl_peso = `val${i}_peso`;
+			if (this.verifica[lbl_peso]) {
+				tot += this.verifica[lbl_peso];
+			}
+		}
+		for (let i = 1; i <= 6; i++) {
+			const lbl_peso = `val${i}_peso`;
+			const lbl = `val${i}`;
+			if (this.verifica[lbl_peso]) {
+				voto += (row[lbl]/4) * (this.verifica[lbl_peso]/tot);
+			}
+		}
+		voto = Math.ceil(voto * 1000) / 100;
+		const penalita = this.penalita(row);
+		return `${voto.toPrecision(3)} - ${penalita} = ${(voto - penalita).toPrecision(3)}`
+	},
+	penalita(row) {
+		let penalita = 0;
+		if (row.dis === 'om') {
+			return 0;
+		}
+		if (row['minuti'] > this.verifica['limite_1']) {
+			penalita += this.verifica.penalita_1;
+		} else
+		if (row['minuti'] > this.verifica['limite_2']) {
+			penalita += this.verifica.penalita_2;
+		}
+		if (row.dis == 'dsa' && penalita > 0) {
+			penalita -= 1;
+		}
+		return penalita;
 	}
 }
