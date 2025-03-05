707362bb55a0e2bdca0d963fda95e342e3adf08b
Lorenzo Principi <l.principi8@gmail.com>
2025-03-05 10:45:56 +0000

    up

---
+++ pages/Public.veral/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,47 @@
+export default {
+	showTimings: true,
+	valOptions: [
+    {
+        "value": 0,
+        "label": "0"
+    },
+    {
+        "value": 0.5,
+        "label": "0.5"
+    },
+    {
+        "value": 1,
+        "label": "1"
+    },
+    {
+        "value": 1.5,
+        "label": "1.5"
+    },
+    {
+        "value": 2,
+        "label": "2"
+    },
+    {
+        "value": 2.5,
+        "label": "2.5"
+    },
+    {
+        "value": 3,
+        "label": "3"
+    },
+    {
+        "value": 3.5,
+        "label": "3.5"
+    },
+    {
+        "value": 4,
+        "label": "4"
+    }
+],
+	switchShowTimings() {
+		this.showTimings = !this.showTimings;
+	},
+	bo(th) {
+		console.log(th);
+	}
+}
