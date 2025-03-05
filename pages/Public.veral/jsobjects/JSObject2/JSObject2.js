451d21be7c25e9a5eae2199b49cbadfe4493dc54
Lorenzo Principi <l.principi8@gmail.com>
2025-03-05 16:31:07 +0000

    update

--- pages/Public.veral/jsobjects/JSObject2/JSObject2.js
+++ pages/Public.veral/jsobjects/JSObject2/JSObject2.js
@@ -1,40 +1,14 @@
 export default {
-	valOptions: [
-    {
-        "value": 0,
-        "label": "0"
-    },
-    {
-        "value": 0.5,
-        "label": "0.5"
-    },
-    {
-        "value": 1,
-        "label": "1"
-    },
-    {
-        "value": 1.5,
-        "label": "1.5"
-    },
-    {
-        "value": 2,
-        "label": "2"
-    },
-    {
-        "value": 2.5,
-        "label": "2.5"
-    },
-    {
-        "value": 3,
-        "label": "3"
-    },
-    {
-        "value": 3.5,
-        "label": "3.5"
-    },
-    {
-        "value": 4,
-        "label": "4"
-    }
-],
+	valOptions: [],
+	setValOptions () {
+		let opts = [];
+		for (let i = 8; i >= 0; i--) {
+			if (i % 2 === 1) {
+				opts.push({ label: `${(i/2).toPrecision(2)}`, value: `${(i/2).toPrecision(2)}`});			
+			} else {
+				opts.push({ label: `${(i/2)}`, value: `${(i/2)}`})
+			}
+		}
+		this.valOptions = opts;
+	}
 }
