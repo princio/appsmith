0452185ebfe9f3ed0915814b7fb4821791bae1ee
Lorenzo Principi <l.principi8@gmail.com>
2025-03-06 15:12:16 +0000

    update

--- pages/Public.veral/jsobjects/JSObject1/JSObject1.js
+++ pages/Public.veral/jsobjects/JSObject1/JSObject1.js
@@ -56,5 +56,34 @@ export default {
 			penalita -= 1;
 		}
 		return penalita;
+	},
+	setTesto () {
+    const vals = {
+        0: 'Errato',
+        1: 'Errato',
+        2: 'Molte Imprecisioni',
+        3: 'Impreciso',
+        4: 'Buono',
+    };
+
+		const x1 = Number(Math.floor(data_table.selectedRow.val1));
+		const x2 = Number(Math.floor(data_table.selectedRow.val2));
+		const x3 = Number(Math.floor(data_table.selectedRow.val3));
+		const x4 = Number(Math.floor(data_table.selectedRow.val4));
+		const x5 = Number(Math.floor(data_table.selectedRow.val5));
+		const x6 = Number(Math.floor(data_table.selectedRow.val6));
+		
+		const n2 = (x) => Number(Math.floor(x));
+
+		const condizioni = x1;
+		const intervalli = x2;
+		const tot_media = (x3 + x6)/2;
+		const conta_se = (x4 + x5)/2;
+
+		this.currentValutazione = 
+			`1) Scrittura precisa degli intervalli: ${vals[n2(intervalli)]}. ` +
+			`2) Scrittura corretta delle condizioni: ${vals[n2(condizioni)]}. ` +
+			`3) Utilizzo CONTA.SE: ${vals[n2(conta_se)]}. ` +
+			`4) Totali e media: ${tot_media < 2 ?  'Non sufficiente' : 'Sufficiente'}.`;
 	}
 }
